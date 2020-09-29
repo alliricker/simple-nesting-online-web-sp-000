9df57261bc6b31d8709f4f2c5776132159c84b28
Allison Ricker <alliricker@gmail.com>
2020-09-29 02:02:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,9 +65,6 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -83,7 +80,7 @@ programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LI
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 
 end
 
