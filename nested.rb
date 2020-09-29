1425945825ec45b3d17887301d4a6a0fd81624c7
Allison Ricker <alliricker@gmail.com>
2020-09-29 02:15:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,7 +79,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = {:known_for, :languages}
+programmer_hash[:yukihiro_matsumoto] = :known_for
+programmer_hash[:yukihiro_matsumoto] = :languages 
 yukihiro_matsumoto[:known_for] = "Ruby"
 yukihiro_matsumoto[:languages] = ["LISP", "C"]
 
